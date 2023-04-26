1754813af269987da2065f65716915b6a6e94857
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2023-04-26 07:16:18 +0500

    Add files via upload

---
+++ js/preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron') 
+
+
+contextBridge.exposeInMainWorld('electronAPI', { 
+    quit: () => ipcRenderer.send('quit'),
+    min: () => ipcRenderer.send('min'),
+    winMax: () => ipcRenderer.send('winMax'),
+    winMin: () => ipcRenderer.send('winMin')
+})
+
+
---
+++ js/render.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    let btnClose = document.querySelector('.btn-close'),
+    btnMin = document.querySelector('.btn-min'),
+    btnMax = document.querySelector('.btn-max'),
+    flagWin = false;
+
+    btnClose.addEventListener('click', () =>{
+        window.electronAPI.quit();
+    })
+
+    btnMin.addEventListener('click', () =>{
+        window.electronAPI.min();
+    })
+
+
+    btnMax.addEventListener('click', () =>{
+        if(!flagWin){
+            window.electronAPI.winMax();
+            document.querySelector('#winbtn').classList.remove('fa-window-maximize');
+            document.querySelector('#winbtn').classList.add('fa-window-restore');
+            flagWin = !flagWin;
+        }else{
+            window.electronAPI.winMin();
+            document.querySelector('#winbtn').classList.remove('fa-window-restore');
+            document.querySelector('#winbtn').classList.add('fa-window-maximize');
+            flagWin = !flagWin;            
+        }
+    })
+
+    
+})
